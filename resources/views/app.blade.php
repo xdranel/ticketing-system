15e0b9ac52b6e061672726c071df1bffbfc0bc20
xdranel <gendhiramona@gmail.com>
2026-09-09 10:13:57 +0700

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Layout</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+
+</body>
+</html>
